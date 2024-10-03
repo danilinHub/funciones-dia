24e66db0cb17dd46f45d591b44ca9abba2940bac
Danilo Collo <dcollo354@gmail.com>
2024-10-03 14:51:37 -0500

    se crea el proyecti con el archivo y una funcion con el hola mmundo

---
+++ funcionesProject/Main.py
@@ -0,0 +1,12 @@
+# Definicion de una funcion
+def my_funcion():
+    print("Hello from a function")
+
+#Invocar la funcion
+my_funcion()
+
+def mostrarNombre(nombre):
+    print("Su nombre es "+nombre)
+
+nom = input("igrese su nombre: ")
+mostrarNombre(nom)
